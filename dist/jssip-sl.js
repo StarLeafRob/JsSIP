1054df9bacfce9e43262caaf50a5ac082ad4641f
rdt <rdt@starleaf.com>
2018-02-13 09:17:31 +0000

    Bump version

--- dist/jssip-sl.js
+++ dist/jssip-sl.js
@@ -1,7 +1,7 @@
 /*
- * JsSIP v3.0.11
+ * JsSIP v3.0.12
  * the Javascript SIP library
- * Copyright: 2012-2017 José Luis Millán <jmillan@aliax.net> (https://github.com/jmillan)
+ * Copyright: 2012-2018 José Luis Millán <jmillan@aliax.net> (https://github.com/jmillan)
  * Homepage: http://jssip.net
  * License: MIT
  */
@@ -23785,7 +23785,7 @@ module.exports={
   "name": "jssip-sl",
   "title": "JsSIP",
   "description": "the Javascript SIP library",
-  "version": "3.0.11",
+  "version": "3.0.12",
   "homepage": "http://jssip.net",
   "author": "José Luis Millán <jmillan@aliax.net> (https://github.com/jmillan)",
   "contributors": [
